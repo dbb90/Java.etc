d3757cb6a087468f1ba57cf46f0c800f95f54d73
dbb90 <dbb090@gmail.com>
2017-08-22 12:45:49 -0400

    DABAINDWRK-3 fixed a wild typo!

--- HelloWorld/src/main/java/App.java
+++ HelloWorld/src/main/java/App.java
@@ -11,7 +11,7 @@
 public class App {
     
     public static void main(String[] args) {
-            System.out.println("Hello Werld");
+            System.out.println("Hello World!");
         
     }
     
